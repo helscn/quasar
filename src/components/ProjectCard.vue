d361ab08157f7a43089826a9386285a112d87f22
helscn <helscns@gmail.com>
2021-06-22 15:05:16 +0800

    将项目卡片分离为单独组件

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,25 +1,70 @@
 <template>
-  <q-item :to="path" clickable v-ripple>
-    <q-item-section avatar>
-      <q-avatar rounded :icon="icon"> </q-avatar>
-    </q-item-section>
-    <q-item-section>
-      <q-item-label :lines="1">{{ name }}</q-item-label>
-      <q-item-label :lines="3" caption>{{ description }}</q-item-label>
-    </q-item-section>
-  </q-item>
+  <q-card class="my-card q-my-sm q-ml-lg" bordered>
+    <q-card-section horizontal>
+      <q-item :to="path" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img :src="icon" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ name }}</q-item-label>
+          <q-item-label caption>
+            {{ creater }} @ {{ created_date }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card-section>
+    <q-card-section class="q-py-sm  ">
+      <q-item-label>
+        {{ description }}
+      </q-item-label>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions class="justify-end q-py-none">
+      <q-btn flat round color="primary" icon="group_add"
+        ><q-tooltip>增加项目组成员</q-tooltip></q-btn
+      >
+      <q-btn flat round color="accent" icon="group_remove"
+        ><q-tooltip>删除项目组成员</q-tooltip></q-btn
+      >
+      <q-btn flat round color="red" icon="favorite"
+        ><q-tooltip>退出项目组</q-tooltip></q-btn
+      >
+      <q-btn flat round color="grey" icon="favorite"
+        ><q-tooltip>加入项目组</q-tooltip></q-btn
+      >
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script>
 export default {
-  name: "ProjectItem",
+  name: "ProjectCard",
   props: {
     path: {
       type: String,
       default: ""
     },
 
+    id: {
+      type: Number
+    },
+
     name: {
+      type: String,
+      default: ""
+    },
+
+    creater: {
+      type: String,
+      default: ""
+    },
+
+    created_date: {
       type: String,
       default: ""
     },
@@ -28,11 +73,12 @@ export default {
       type: String,
       default: ""
     },
-
     icon: {
       type: String,
-      default: "pending_actions"
+      default: "/images/Avatar.png"
     }
   }
 };
 </script>
+
+<style scoped></style>
--- src/pages/ProjectsView.vue
+++ src/pages/ProjectsView.vue
@@ -1,243 +1,60 @@
 <template>
   <q-page>
-    <div class="q-ma-sm row items-start q-gutter-md">
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item to="/table" clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="/images/Avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title 这是标题</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section class="q-py-sm  ">
-          <q-item-label>
-            这是一个测试，用于展示。
-          </q-item-label>
-        </q-card-section>
-
-        <q-separator />
-
-        <q-card-actions class="justify-end q-py-none">
-          <q-btn flat round color="primary" icon="group_add"
-            ><q-tooltip>增加项目组成员</q-tooltip></q-btn
-          >
-          <q-btn flat round color="accent" icon="group_remove"
-            ><q-tooltip>删除项目组成员</q-tooltip></q-btn
-          >
-          <q-btn flat round color="red" icon="favorite"
-            ><q-tooltip>退出项目组</q-tooltip></q-btn
-          >
-          <q-btn flat round color="grey" icon="favorite"
-            ><q-tooltip>加入项目组</q-tooltip></q-btn
-          >
-        </q-card-actions>
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar icon="article"> </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
-      <q-card class="my-card q-ma-sm" bordered>
-        <q-card-section horizontal>
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Title</q-item-label>
-              <q-item-label caption>
-                Subhead
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-card-section>
-        <q-card-section>
-          <q-item-label>
-            这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。
-          </q-item-label>
-        </q-card-section>
-        <q-card-actions class="justify-end">
-          <q-btn flat round color="red" icon="favorite" />
-          <q-btn flat round color="accent" icon="bookmark" />
-          <q-btn flat round color="primary" icon="share" />
-        </q-card-actions>
-
-        <q-separator />
-      </q-card>
+    <div class="q-ma-sm row items-start content-start q-gutter-md">
+      <ProjectCard v-for="item in projectsList" :key="item.id" v-bind="item" />
     </div>
   </q-page>
 </template>
 
 <script>
+import ProjectCard from "components/ProjectCard.vue";
 export default {
+  components: { ProjectCard },
   data() {
     return {
       projectsList: [
         {
+          id: 1,
           name: "测试项目",
           description: "这是项目描述，说明项目的背景及制作要求。",
+          creater: "何罗生",
+          created_date: "2021/23/23 08:21:22",
           path: "/table"
         },
         {
+          id: 2,
+          name: "测试项目",
+          description: "这是项目描述，说明项目的背景及制作要求。",
+          creater: "何罗生",
+          created_date: "2021/23/23 08:21:22",
+          path: "/table"
+        },
+        {
+          id: 3,
           name: "Can you Mary me, my beautiful princess",
           description:
             "这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。",
-          path: "/test"
+          creater: "何罗生",
+          created_date: "2021/23/23 08:21:22",
+          path: "/table"
+        },
+        {
+          id: 4,
+          name: "Can you Mary me, my beautiful princess",
+          description:
+            "这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。",
+          creater: "何罗生",
+          created_date: "2021/23/23 08:21:22",
+          path: "/table"
+        },
+        {
+          id: 5,
+          name: "Can you Mary me, my beautiful princess",
+          description:
+            "这是一个测试，用于展示显示测试内容震左吉大南校大方地说安抚加大开发地地载需要在地革震奈斯。",
+          creater: "何罗生",
+          created_date: "2021/23/23 08:21:22",
+          path: "/table"
         }
       ]
     };
@@ -247,6 +64,6 @@ export default {
 
 <style scoped>
 .my-card {
-  max-width: 300px;
+  width: 350px;
 }
 </style>
